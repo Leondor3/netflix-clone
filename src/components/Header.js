6e2b927fc7ca2b33c5a02189bdb2925c06c9b346
Leandro <leandro.a.santos@hotmail.com>
2021-08-31 23:02:18 -0300

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'; 
+import './Header.css';
+export default () => {
+    
+    return(
+      <header> 
+          <div className="header--logo">
+              <a href = "/">
+                  <img src = "https://t2.tudocdn.net/178296?w=660&h=660"/>
+              </a>
+          </div>
+      </header>
+    );
+}
